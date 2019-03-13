b56a2b3bb5075d9e48164aac44b70c58f5997d37
Dedi Hartono <dedihartono1993@gmail.com>
2019-03-13 23:01:11 +0700

    Setup Package

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://root:dedi123A@dehartcluster-0himb.mongodb.net/test?retryWrites=true",
+  secretOrKey: "secret"
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const welcome = require("./routes/api/welcome");
+const users = require("./routes/api/users");
+
+const app = express();
+
+app.use("/", welcome);
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
